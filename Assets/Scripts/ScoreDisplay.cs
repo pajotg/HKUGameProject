facc8c45fbb8da9afab47960e578e0041953d607
Pajotg <a@a.com>
2025-08-27 16:09:00 +0200

    score display using nice numbers

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -1,17 +1,45 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreDisplay : MonoBehaviour
 {
-    public bool HideIfZeroScore = false;
+
+    public bool HideIfZero;
+
+    [Tooltip("Sprites for digits 0-9")] 
+    public Sprite[] digits;
+    [Tooltip("Prefab with an Image component for a digit")] 
+    public GameObject digitPrefab;
+    [Tooltip("Spacing between digits")] 
+    public float digitSpacing = 40f;
+
+
     void Update()
     {
-        var text = GetComponent<TMPro.TextMeshProUGUI>();
-        if (HideIfZeroScore && ScoreCounter.Score == 0)
+        var score = ScoreCounter.Score;
+        string scoreStr = score.ToString();
+
+        // Remove old digits
+        foreach (Transform child in transform)
         {
-            text.text = "";
-            return;
+            Destroy(child.gameObject);
         }
         
-        text.text = $"Score: {ScoreCounter.Score}";
+        if (score == 0 && HideIfZero) { return; }
+
+        // Instantiate new digits
+        for (int i = 0; i < scoreStr.Length; i++)
+        {
+            int digit = scoreStr[i] - '0';
+            if (digit < 0 || digit > 9) continue;
+            GameObject go = Instantiate(digitPrefab, transform);
+            var img = go.GetComponent<UnityEngine.UI.Image>();
+            if (img && digits.Length > digit)
+                img.sprite = digits[digit];
+            // Position digit
+            var rt = go.GetComponent<RectTransform>();
+            if (rt)
+                rt.anchoredPosition = new Vector2((i - (float)(scoreStr.Length - 1) / 2) * digitSpacing, 0);
+        }
     }
 }
